014ae00acc213bad2ae552dea02bd221f0396d28
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-05-02 22:19:36 +0530

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -49,24 +49,14 @@ cmd({
     // Send the video with details
     await conn.sendMessage(from, {
       video: videoBuffer,
-      caption: `*🫟𝐀ɭīī 𝐌Ɗ 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐃*\n\n` +
+      caption: `*𝐅𝖾ᥣ𝗂𝗑 𝐊𝗂 𝐓α𝗋α𝖿 𝐒𝖾*\n\n` +
         `🔖 *ᴛɪᴛʟᴇ*: ${title || "No title"}\n` +
         `👤 *ᴀᴜᴛʜᴏʀ*: ${author.nickname}\n` +
         `♥️ *ʟɪᴋᴇs*: ${metrics.digg_count}\n` +
         `💬 *ᴄᴏᴍᴍᴇɴᴛs*: ${metrics.comment_count}\n` +
         `♻️ *sʜᴀʀᴇs*: ${metrics.share_count}\n` +
         `📥 *ᴅᴏᴡɴʟᴏᴀᴅs*: ${metrics.download_count}\n\n` +
-        `> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀʟɪ*`,
-      contextInfo: {
-        mentionedJid: [m.sender],
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363318387454868@newsletter',
-          newsletterName: '『 𝐀ɭīī 𝐌Ɗ 𝐒ʊ̊𝐏𝐏๏፝֟ɼʈ 』',
-          serverMessageId: 143
-        }
-      }
+        `> *𝐅𝖾ᥣ𝗂𝗑*`
     }, { quoted: mek });
 
     // Add a reaction to indicate success
