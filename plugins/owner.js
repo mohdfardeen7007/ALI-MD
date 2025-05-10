4f83c6715474b544e90b4060e49b26bf58329191
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-05-10 10:01:20 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -37,7 +37,7 @@ async (conn, mek, m, { from }) => {
         await conn.sendMessage(from, {
             text: `This is the owner's contact: ${ownerName}`,
             contextInfo: {
-                mentionedJid: [ownerNumber.replace('+923003588997') + '+923003588997@s.whatsapp.net'], // Mention the owner
+                mentionedJid: [ownerNumber.replace('+918127875972') + '+923003588997@s.whatsapp.net'], // Mention the owner
                 quotedMessageId: sentVCard.key.id // Reference the vCard message
             }
         }, { quoted: mek });
