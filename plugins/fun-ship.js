2f66a5c751f2bdc34dcf9eb881a4157f87e2c54b
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-05-26 11:13:28 +0530

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -31,19 +31,7 @@ cmd({
 
     const message = `💘 *Match Found!* 💘\n❤️ @${sender.split("@")[0]} + @${randomPair.split("@")[0]}\n💖 Congratulations! 🎉`;
 
-    await conn.sendMessage(from, {
-      text: message,
-      contextInfo: {
-        mentionedJid: [sender, randomPair],
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: "120363318387454868@newsletter",
-          newsletterName: "𝐀ɭι̇ι̇ 𝐌Ɗ 🍁",
-          serverMessageId: 143
-        }
-      }
-    });
+    
 
   } catch (error) {
     console.error("❌ Error in ship command:", error);
